5b5884c5c9dc1671fa256c516c16bc0b038e084b
Olival <olivalpaulinocc@gmail.com>
2023-03-25 17:31:52 -0300

    Adicionando os livros Quem Pensa Enriquece e Mais Esperto que o Diabo

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,22 +18,26 @@ import livro_o_homem_mais_rico_da_babilonia from '../img/o_homem_mais_rico_da_ba
 import livro_arrume_sua_cama from '../img/arrume_sua_cama.jpg';
 import livro_foco from '../img/foco.jpg';
 import livro_voce_pode_curar_sua_vida from '../img/voce_pode_curar_sua_vida.jpg';
+import livro_quem_pensa_enriquece from '../img/quem_pensa_enriquece.jpg';
+import livro_mais_esperto_que_o_diabo from '../img/mais_esperto_que_o_diabo.jpg';
 
 const Card = () => {
     let nome_autor = ["Richard Dawkins", "Dale Carnegie", "Robert T. Kiyosaki", "Hastings e Meyer", "Daniel Kahneman", "Ryan Holiday",
-    "Hal Elrod", "Brené Brown","T. Harv", "George S.", "William H.", "Daniel Goleman", "Louise L."];
+    "Hal Elrod", "Brené Brown","T. Harv", "George S.", "William H.", "Daniel Goleman", "Louise L.", "Napoleon Hill", "Napoleon Hill"];
     
     let alt_imagem = ["O Gene Egoísta, Richard Dawkins", "Como fazer amigos e influenciar pessoas, Dale Carnegie", "Pai Rico, Pai Pobre", "A Regra é Não Ter Regras", 
     "Rápido e Devagar", "O Ego é Seu Inimigo", "O Milagre da Manhã", "A Coragem de Ser Imperfeito", "Os Segredos da Mente Milionária",
-    "O Homem Mais Rico da Babilônia", "Arrume Sua Cama", "Foco", "Você Pode Curar Sua Vida"]
+    "O Homem Mais Rico da Babilônia", "Arrume Sua Cama", "Foco", "Você Pode Curar Sua Vida", "Quem Pensa Henriquece", "Mais Esperto que o Diabo"]
     
     let url = ["https://amzn.to/3nn7mSI", "https://amzn.to/40foeJB", "https://amzn.to/3JJj8Oz", "https://amzn.to/3nkh2gC", "https://amzn.to/3LRp0YJ",
     "https://amzn.to/3lAGGNT", "https://amzn.to/3JNmKzh", "https://amzn.to/3K98ySu", "https://amzn.to/40m570r", "https://amzn.to/3z7r07N",
-    "https://amzn.to/3FS7dNh", "https://amzn.to/3LQDWGw", "https://amzn.to/40k0q7m"];
+    "https://amzn.to/3FS7dNh", "https://amzn.to/3LQDWGw", "https://amzn.to/40k0q7m", "https://amzn.to/3ZiNMV1", "https://amzn.to/40CTHFe",
+    ];
     
     let url_imagem = [livro_ogene_egoista, livro_como_faz_amigos, livro_pai_rico_pai_pobre, livro_a_regra_e_nao_ter_regra, livro_rapido_e_devagar,
         livro_o_ego_e_seu_inimigo, livro_o_milagre_da_manha, livro_a_coragem_de_ser_imperfeito, livro_os_segredos_da_mente_milionaria,
-        livro_o_homem_mais_rico_da_babilonia, livro_arrume_sua_cama, livro_foco, livro_voce_pode_curar_sua_vida];
+        livro_o_homem_mais_rico_da_babilonia, livro_arrume_sua_cama, livro_foco, livro_voce_pode_curar_sua_vida, livro_quem_pensa_enriquece, 
+        livro_mais_esperto_que_o_diabo];
 
   return (
     <div class="cards">
@@ -50,8 +54,8 @@ const Card = () => {
         <Imagem url={url[10]} url_imagem={url_imagem[10]} alt_imagem={alt_imagem[10]} nome_autor={nome_autor[10]} />
         <Imagem url={url[11]} url_imagem={url_imagem[11]} alt_imagem={alt_imagem[11]} nome_autor={nome_autor[11]} />
         <Imagem url={url[12]} url_imagem={url_imagem[12]} alt_imagem={alt_imagem[12]} nome_autor={nome_autor[12]} />
-        <Imagem url={url[0]} url_imagem={url_imagem[0]} alt_imagem={alt_imagem[0]} nome_autor={nome_autor[0]} />
-        <Imagem url={url[0]} url_imagem={url_imagem[0]} alt_imagem={alt_imagem[0]} nome_autor={nome_autor[0]} />
+        <Imagem url={url[13]} url_imagem={url_imagem[13]} alt_imagem={alt_imagem[13]} nome_autor={nome_autor[13]} />
+        <Imagem url={url[14]} url_imagem={url_imagem[14]} alt_imagem={alt_imagem[14]} nome_autor={nome_autor[14]} />
         <Imagem url={url[0]} url_imagem={url_imagem[0]} alt_imagem={alt_imagem[0]} nome_autor={nome_autor[0]} />
         <Imagem url={url[0]} url_imagem={url_imagem[0]} alt_imagem={alt_imagem[0]} nome_autor={nome_autor[0]} />
         <Imagem url={url[0]} url_imagem={url_imagem[0]} alt_imagem={alt_imagem[0]} nome_autor={nome_autor[0]} />
